fc631361e9a02cc699932fdb65485708e582fe19
gautambedi1998 <41571537+gautambedi1998@users.noreply.github.com>
2018-10-14 21:18:49 +1100

    Add files via upload

---
+++ working_code.ino
@@ -0,0 +1,32 @@
+//so here is the code for the automated light system that will turn on and off the lights  
+
+int soundSensor=2;
+int LED=4;
+boolean LEDStatus=false;
+
+void setup() {
+  Serial.begin(9600);
+ pinMode(soundSensor,INPUT);
+ pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+
+  int SensorData=digitalRead(soundSensor); 
+     Serial.println( SensorData);
+  if(SensorData==1){
+
+    if(LEDStatus==false){
+        LEDStatus=true;
+        digitalWrite(LED,HIGH);
+        {
+         Serial.println( SensorData);
+        }
+    }
+    else{
+        LEDStatus=false;
+        digitalWrite(LED,LOW);
+    }
+  }
+ } 
